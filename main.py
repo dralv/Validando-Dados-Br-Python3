e55cc4d3ac93b0d3fc44875520f0f00cb5bc4771
Álvaro Teixeira de Araújo <dralvphd2@gmail.com>
2022-04-14 19:34:43 -0300

    Validando CNPJ

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
-from Cpf import Cpf
+from Cpf_cnpj import CpfCnpj
+from validate_docbr import CNPJ
 
-cpf_um = Cpf("06146174300")
+#cpf_um = Cpf("06146174300")
 
-print(cpf_um)
+#print(cpf_um)
 
+exemplo_cnpj = "35379838000112"
+#cnpj = CNPJ()
 
+#print(cnpj.validate(exemplo_cnpj))
 
+documento = CpfCnpj(exemplo_cnpj,'cnpj')
 
 
 
